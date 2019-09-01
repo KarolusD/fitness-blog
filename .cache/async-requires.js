9fa203c1726eb9c00f38986e5f5d91965c62fe31
Karol Podżerek <karolus@JARVIS.local>
2019-09-01 21:11:15 +0200

    added all sections and smooth scrolling

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/karolus/Documents/Development/Fitness-blog/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/Users/karolus/Documents/Development/Fitness-blog/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/Users/karolus/Documents/Development/Fitness-blog/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
